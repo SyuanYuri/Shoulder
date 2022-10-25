ebcac5b63610af2fe4bee3ff92c23dce2d542fcb
SyuanYu <syuanyuri@gmail.com>
2022-10-25 11:55:45 +0800

    update Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <script></script>
 
 <template>
-  <footer class="container-fluid">
+  <footer class="container-fluid p-0">
     <div class="row">
       <div class="col-6">
 
@@ -35,44 +35,61 @@
         </ul>
       </div>
     </div>
+    <h5 class="copyright">
+      Copyright © 2022 Shoulders Foundation. All rights reserved
+    </h5>
   </footer>
 </template>
 
 <style lang="scss">
 footer {
-  padding: 70px 0;
   color: #fff;
   letter-spacing: 2px;
   background-color: var(--bg-color);
 
-  ul {
-    padding: 0;
-    list-style: none;
+  .row {
+    padding: 70px 0;
 
-    i {
-      margin-right: 15px;
-      font-size: 20px;
-      color: #fff;
+    ul {
+      padding: 0;
+      list-style: none;
+
+      i {
+        margin-right: 10px;
+        font-size: 20px;
+        color: #fff;
+      }
+
     }
 
-  }
+    .media {
+      i {
+        margin-right: 15px;
+        font-size: 40px;
+        color: var(--sub-color);
 
-  .media {
-    i {
-      margin-right: 15px;
-      font-size: 40px;
-      color: var(--sub-color);
-      &:hover {
-        opacity: .8;
+        &:hover {
+          opacity: .8;
+        }
       }
     }
+
+    span {
+      display: block;
+      font-size: 33px;
+      font-weight: 100;
+      margin-bottom: 20px;
+    }
   }
 
-  span {
-    display: block;
-    font-size: 33px;
-    font-weight: 100;
-    margin-bottom: 20px;
+  .copyright {
+    margin: 0;
+    padding: 10px;
+    text-align: center;
+    font-size: 16px;
+    color: var(--bg-color);
+    background-color: var(--sub-color);
+    letter-spacing: 1px;
   }
 }
 </style>
